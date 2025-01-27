eb05ad484572555507d6941e97091db52199abd1
mc00f <125220859+mc00f@users.noreply.github.com>
2025-01-27 06:10:42 -0500

    Update bl.lua

--- bl.lua
+++ bl.lua
@@ -1,6 +1,6 @@
-local bl = {
-  ["Entity"] = "Roblox"
-  ["ID"] = 1
-}
-return bl
--- i have no idea what im doing so dont judge x
+game.Players.PlayerAdded:Connect(function(player)
+    local bannedIds = {1234, 5678, 9012} -- put the banned player IDs in this table
+    if table.find(bannedIds, player.UserId) then
+        player:Kick("AC | You have been banned from the game.")
+    end
+end)
